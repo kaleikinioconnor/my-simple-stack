b336871b138452461d25ab539e49d3aa1ecfe33c
kaleikinioconnor <onnorkini@gmail.com>
2016-11-14 08:12:20 +0100

    #4 exigence 4 update :d

---
+++ src/main/java/deco/kocon/Item.java
@@ -0,0 +1,24 @@
+package deco.kocon;
+
+/**
+ * Created by Kini on 14/11/2016.
+ */
+public class Item {
+
+    Private Object value;
+
+
+    Item(Object value){
+        this.value = value;
+    }
+
+    public object getValue(){
+        return value;
+    }
+
+    /**
+     * new jjno,ok EXIGENCE 3
+     */
+}
+
+
